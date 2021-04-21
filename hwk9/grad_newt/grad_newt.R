8c80ca19e854a20b24ea9eb69e68ed5dd7e45ec4
YYY <XXX>
2021-04-20 22:47:37 -0500

    fix hwk9 newt_grad hessian and wrap that problem up
    still missing the efficient sol to portfolio (inefficient recs)
    still missing gravity feed water supply network problem

--- hwk9/grad_newt/grad_newt.R
+++ hwk9/grad_newt/grad_newt.R
@@ -1,3 +1,6 @@
+.libPaths(c("/usr/lib/R/library", "~/rlibs"))
+library(ggplot2)
+library(data.table)
 ## maxIter <- 10
 ## n <- 3; m <- 5
 maxIter <- 1000
@@ -20,7 +23,18 @@ getGrad <- function(A, x) {
 }
 
 getHess <- function(A, x) {
-    sum(1/(1 - A %*% x)^2) * (t(A) %*% A) + diag(2*(x^2 + 1)/(1-x^2)^2)
+    ## sum(1/(1 - A %*% x)^2) * (t(A) %*% A) #+ diag(2*(x^2 + 1)/(1-x^2)^2)
+    t(A) %*% diag(as.numeric(1 / (-A %*% x + 1)^2)) %*% A + diag(2*(x^2 + 1)/(1-x^2)^2)
+}
+
+getWolfHess <- function(A, x) { #debug hessian with wolfram alpha
+    ## Atest <- matrix(c(c(1,2,3),c(4,5,6)), nrow = 2, byrow = TRUE)
+    ## {{-16/(1 - 4 x - 5 y - 6 z)^2 - (1 - x - 2 y - 3 z)^(-2), -20/(1 - 4 x - 5 y - 6 z)^2 - 2/(1 - x - 2 y - 3 z)^2, -24/(1 - 4 x - 5 y - 6 z)^2 - 3/(1 - x - 2 y - 3 z)^2}, {-20/(1 - 4 x - 5 y - 6 z)^2 - 2/(1 - x - 2 y - 3 z)^2, -25/(1 - 4 x - 5 y - 6 z)^2 - 4/(1 - x - 2 y - 3 z)^2, -30/(1 - 4 x - 5 y - 6 z)^2 - 6/(1 - x - 2 y - 3 z)^2}, {-24/(1 - 4 x - 5 y - 6 z)^2 - 3/(1 - x - 2 y - 3 z)^2, -30/(1 - 4 x - 5 y - 6 z)^2 - 6/(1 - x - 2 y - 3 z)^2, -36/(1 - 4 x - 5 y - 6 z)^2 - 9/(1 - x - 2 y - 3 z)^2}}
+
+    row1 <- c(-16/(1 - 4*x[1] - 5*x[2] - 6*x[3])^2 - (1 -x[1] - 2*x[2] - 3*x[3])^(-2), -20/(1 - 4*x[1] - 5*x[2] - 6*x[3])^2 - 2/(1 -x[1] - 2*x[2] - 3*x[3])^2, -24/(1 - 4*x[1] - 5*x[2] - 6*x[3])^2 - 3/(1 -x[1] - 2*x[2] - 3*x[3])^2)
+    row2 <- c(-20/(1 - 4*x[1] - 5*x[2] - 6*x[3])^2 - 2/(1 -x[1] - 2*x[2] - 3*x[3])^2, -25/(1 - 4*x[1] - 5*x[2] - 6*x[3])^2 - 4/(1 -x[1] - 2*x[2] - 3*x[3])^2, -30/(1 - 4*x[1] - 5*x[2] - 6*x[3])^2 - 6/(1 -x[1] - 2*x[2] - 3*x[3])^2)
+    row3 <- c(-24/(1 - 4*x[1] - 5*x[2] - 6*x[3])^2 - 3/(1 -x[1] - 2*x[2] - 3*x[3])^2, -30/(1 - 4*x[1] - 5*x[2] - 6*x[3])^2 - 6/(1 -x[1] - 2*x[2] - 3*x[3])^2, -36/(1 - 4*x[1] - 5*x[2] - 6*x[3])^2 - 9/(1 -x[1] - 2*x[2] - 3*x[3])^2)
+    return(-matrix(c(row1, row2, row3), nrow = 3, byrow = TRUE))
 }
 
 bakTrak <- function(x, g, dir, alpha, beta) {
@@ -79,7 +93,7 @@ runGradientDescent <- function() {
 
 y <- runGradientDescent()
 ## ggplot(y, aes(x=i, y=v)) + geom_point() + geom_line()
-ggplot(y, aes(x=i, y=abs(ans-v))) + geom_point() + geom_line()
+## ggplot(y, aes(x=i, y=abs(ans-v))) + geom_point() + geom_line()
 
 runNewtonDescent <- function() {
     x <- rep(0,n)
@@ -111,5 +125,9 @@ runNewtonDescent <- function() {
 }
 
 z <- runNewtonDescent()
-## ggplot(y, aes(x=i, y=v)) + geom_point() + geom_line()
-## ggplot(y, aes(x=i, y=abs(ans-v))) + geom_point() + geom_line()
+## ggplot(z, aes(x=i, y=v)) + geom_point() + geom_line()
+## ggplot(z, aes(x=i, y=abs(ans-v))) + geom_point() + geom_line()
+
+# compare methods
+ggplot(rbindlist(list(y[,.(i, v, mth="G")], z[,.(i, v, mth="N")], data.table(i=32:544, v=tail(z$v,1), mth="N")))[i < 10], aes(x=i, y=v, col=mth)) + geom_point() + geom_line()
+
